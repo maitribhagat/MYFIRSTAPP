cf5dc5bf61f821a2641a1b3ae72443308aa08cb5
Maitri Bhagat <maitri26july@gmail.com>
2024-05-30 21:54:46 +0530

    Initial Commit

---
+++ myfirstapp/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'in28minutes', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Marksons', 'Get DeveOps Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Google', 'Get Azure Certified', CURRENT_DATE(), false);
